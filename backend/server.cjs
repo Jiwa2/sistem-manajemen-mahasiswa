7488ceb805b5a024cd6ce10edbfae6b82e11f622
Jiwa2 <mjiwaislamutidar@gmail.com>
2025-12-18 14:45:54 +0700

    server

--- backend/server.cjs
+++ backend/server.cjs
@@ -99,6 +99,6 @@ app.get("/api/mahasiswa/search/:nim", (req, res) => {
 });
 
 // const PORT = 5000;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("Server running on port " + PORT));
 
